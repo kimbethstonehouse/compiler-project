0f833b98055a8d9a2ab3c52df1c54a08fbf39dd2
kimbethstonehouse <kimbethstonehouse@gmail.com>
2019-10-23 15:12:02 +0100

    update

--- tests/sem/fail_illegal_shadowing.c
+++ tests/sem/fail_illegal_shadowing.c
@@ -1,11 +1,19 @@
 // EXPECTED FAIL
 // new scope then return to old scope
+//int x;
+//
+//void y() {
+//    char c;
+//}
+//
+//void x() {
+//    int p;
+//}
+
+// duplicate var decl
+//int x;
+//int x;
+
+//// var and func same name
 int x;
-
-void y() {
-    char c;
-}
-
-void x() {
-    int p;
-}
+int x() {}
